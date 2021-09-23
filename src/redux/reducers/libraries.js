b48673a45064ddac3a64113286cd6c2a8c9a9542
SkillfactoryCoding <65289009+SkillfactoryCoding@users.noreply.github.com>
2021-09-23 14:34:38 +0300

    Add files via upload

---
+++ src/redux/reducers/libraries.js
@@ -0,0 +1,14 @@
+const initialState = ['redux', 'react'];
+
+function libraries(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_LIBRARY':
+      return [...state, action.payload];
+    case 'DELETE_LIBRARY':
+      return state;
+    default:
+      return state;
+  }
+}
+
+export default libraries;

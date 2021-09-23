3bee8a64eba61e8a0ecf88bdc33da87a3d78d481
SkillfactoryCoding <65289009+SkillfactoryCoding@users.noreply.github.com>
2021-09-23 14:36:16 +0300

    Add files via upload

---
+++ src/routing/old-fashion-route.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class OldFashionRoute extends React.Component {
+  render() {
+    return (
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+      </ul>
+    );
+  }
+}
+
+export default OldFashionRoute;

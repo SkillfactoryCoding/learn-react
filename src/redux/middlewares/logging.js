68e5cd2b9b2c01afb160df07eccba6fc90a3a8ba
SkillfactoryCoding <65289009+SkillfactoryCoding@users.noreply.github.com>
2021-09-23 14:33:47 +0300

    Add files via upload

---
+++ src/redux/middlewares/logging.js
@@ -0,0 +1,14 @@
+// ES5 syntax
+// export function logging(store) {
+//     return function(next) {
+//         return function (action) {
+//             console.log('123')
+//             return next(action)
+//         }
+//     }
+// }
+
+export const logging = store => next => action => {
+  console.log(`Done: ${action.type}`);
+  return next(action);
+};

ef28dfdd1f618431dc6e35cdd9eeabf72a1470e7
SkillfactoryCoding <65289009+SkillfactoryCoding@users.noreply.github.com>
2021-09-23 14:35:50 +0300

    Add files via upload

---
+++ src/routing/react-router/nav-info.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+
+const ShowLocationInfo = ({ match, location, history }) => (
+  <div>You are now at "{location.pathname}"</div>
+);
+
+const ShowLocationInfoWithRouter = withRouter(ShowLocationInfo);
+
+export default ShowLocationInfoWithRouter;

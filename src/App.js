c787396d25ba62003449e398a5a0e133e06af6e1
SkillfactoryCoding <65289009+SkillfactoryCoding@users.noreply.github.com>
2021-09-23 14:38:18 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// import styled, { css } from 'styled-components'
+
+// import BlogPost from "./containers/BlogPostContainer/BlogPost/BlogPost";
+// import TodoList from "./containers/LiftingUpState/TodoList";
+// import TodoList from "./components/TodoList";
+// import { InputWithNameValidation, InputWithNameAndEmailValidation } from "./patterns/render-prop/input/wrapper";
+// import { InputWithNameValidation, InputWithNameAndEmailValidation } from "./patterns/hoc/input/withRequired";
+
+// import OldFashionRoute from "./routing/old-fashion-route";
+// import HistoryAPIRoute from "./routing/history-api-route";
+// import ReactRouterRoute from "./routing/react-router-route";
+// import Routing from "./routing/react-router/routing";
+// import TodoListWithHooks from "./hooks/base-actions/TodoListWithHooks";
+
+// import List from "./styled-components-examples/ToDo/List";
+// import List from "./styled-components-examples/ToDo-reflexbox/List";
+// import StyledComponents from "./styled-components-examples/simple-example";
+// import StyledComponents from "./styled-components-examples/simple-example";
+
+// import Form from './forms/Form';
+// import DiverseForm from './forms/DiverseForm';
+// import UseRefComponent from './forms/UseRefComponent';
+
+// import PropDrilling from './context/PropDrilling';
+// import ClassContext from './context/ClassContext';
+// import FunctionContext from './context/FunctionContext';
+
+const App = () => <div>{/* Place for component here */}</div>;
+export default App;
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import ReduxApp from "./App-redux";
+import { Provider } from 'react-redux';
+
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducer from './redux/reducers';
+import { logging } from './redux/middlewares/logging';
+
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(logging),
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__(),
+  ),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
